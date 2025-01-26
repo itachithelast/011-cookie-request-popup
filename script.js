ad95da04c455c5128d31714c4587ec99dd9cb147
Hamed <h.namazi91@gmail.com>
2025-01-26 12:57:55 +0330

    Show dialog on load

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const btns = document.getElementsByClassName("nav-link")
 const title = document.getElementById("title")
+const dialog = document.querySelector("dialog")
 
 for (i=0 ; i<btns.length ; i++){
     btns[i].addEventListener("click",(e)=>{
         title.innerText = `${e.target.innerText} Content`
     })
 }
+
+window.onload = ()=> dialog.showModal()
