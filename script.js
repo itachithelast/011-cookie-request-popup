d6d635b1101524b62f12783229185fd88fdc341d
Hamed <h.namazi91@gmail.com>
2025-01-26 14:01:12 +0330

    Store dialog answer in cookies

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const btns = document.getElementsByClassName("nav-link")
 const title = document.getElementById("title")
 const dialog = document.querySelector("dialog")
+const acceptBtn = document.querySelector(".btn")
+const closeBtn = document.querySelector("#close")
+const localCookie = document.cookie
 
+
+acceptBtn.addEventListener("click",acceptedDialog)
+closeBtn.addEventListener("click",closeDialog)
 for (i=0 ; i<btns.length ; i++){
     btns[i].addEventListener("click",(e)=>{
         title.innerText = `${e.target.innerText} Content`
     })
 }
 
-window.onload = ()=> dialog.showModal()
+function closeDialog (){
+    dialog.close()
+}
+function acceptedDialog (){
+    document.cookie = "cookie_accepted = 1"
+    closeDialog()
+}
+
+
+if (localCookie == "cookie_accepted=1" ){
+    dialog.close()
+}else{
+    window.onload = ()=> dialog.showModal()
+}
